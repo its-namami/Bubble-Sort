3f15bb59a7fb4965e43ddd95ff1e4e0a1c360fbc
its-namami <164639260+its-namami@users.noreply.github.com>
2025-10-25 13:55:31 +0200

    refactor: separate logic flow

--- main.rb
+++ main.rb
@@ -1,12 +1,17 @@
 unsorted_array = [7, 3, 2, 56, 1, 9, 4]
 
-def bubble_sort(array)
-  comparing_items = array[(0..1)]
+def swap_if_larger(comparing_items)
   if comparing_items.first > comparing_items.last
-    array[0], array[1] = array[1], array[0]
+    comparing_items[0], comparing_items[1] = comparing_items[1], comparing_items[0]
   end
 
-  array
+  comparing_items
+end
+
+def bubble_sort(array)
+  comparing_items = array[(0..1)]
+
+  array = swap_if_larger(comparing_items) + array[(2..-1)]
 end
 
 p bubble_sort(unsorted_array)
